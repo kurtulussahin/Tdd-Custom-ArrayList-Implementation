1d1d7449b417b0983937298e8e6ff778ba3a52d0
kurtulussahin <kurtulussahin@gmail.com>
2024-10-14 22:11:35 +0300

    add method added with simplest way

--- src/main/java/customarraylist/TddArrayList.java
+++ src/main/java/customarraylist/TddArrayList.java
@@ -1,4 +1,24 @@
 package customarraylist;
 
-public class TddArrayList {
+import java.util.AbstractList;
+
+public class TddArrayList<E> extends AbstractList<E> {
+
+    private int size=0;
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    @Override
+    public void add(int index, E element) {
+        size++;
+    }
 }
--- src/test/java/customarraylist/TddCustomArrayListImplementationTest.java
+++ src/test/java/customarraylist/TddCustomArrayListImplementationTest.java
@@ -1,12 +1,31 @@
 package customarraylist;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
- class TddCustomArrayListImplementationTest {
+import java.util.List;
 
-     @Test
-     void implementList(){
+import static org.assertj.core.api.Assertions.assertThat;
 
+class TddCustomArrayListImplementationTest {
+
+    @Test
+    void implementList(){
+        List<String> list =new TddArrayList<>();
+    }
+
+    @Test
+    void emptyListHasSizeZero(){
+         List<String> list =new TddArrayList<>();
+
+         assertThat(list).hasSize(0);
      }
+
+    @Test
+    void listWithOneElementHasSizeOne(){
+        List<String> list =new TddArrayList<>();
+        list.add(RandomStringUtils.random(10));
+        assertThat(list).hasSize(1);
+    }
 
 }
