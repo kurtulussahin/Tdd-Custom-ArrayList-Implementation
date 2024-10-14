23e854cbc5fcf19588aa0c4edab30303924b7b77
kurtulussahin <kurtulussahin@gmail.com>
2024-10-14 23:21:36 +0300

    remove method implemented functionally

--- src/main/java/customarraylist/TddArrayList.java
+++ src/main/java/customarraylist/TddArrayList.java
@@ -2,13 +2,20 @@ package customarraylist;
 
 import java.util.AbstractList;
 
+@SuppressWarnings("unchecked")
 public class TddArrayList<E> extends AbstractList<E> {
 
+    private final E[] backingArray;
     private int size=0;
+
+    public TddArrayList() {
+        this.backingArray=(E[])new Object[10];
+        this.size = 0;
+    }
 
     @Override
     public E get(int index) {
-        return null;
+        return backingArray[index];
     }
 
     @Override
@@ -16,9 +23,32 @@ public class TddArrayList<E> extends AbstractList<E> {
         return size;
     }
 
-
     @Override
     public void add(int index, E element) {
+
+        for(int i=size-1; i>=index; i--){
+            backingArray[i+1]=backingArray[i];
+        }
+        backingArray[index]=element;
         size++;
+    }
+
+    @Override
+    public E remove(int index) {
+
+        E removedElement = backingArray[index];
+
+        for(int i=index; i<size-1; i++){
+
+                backingArray[i] = backingArray[i + 1];
+        }
+
+        backingArray[size-1]=null;
+        size--;
+        return removedElement;
+    }
+
+    int currentCapacity(){
+        return backingArray.length;
     }
 }
--- src/test/java/customarraylist/TddCustomArrayListImplementationTest.java
+++ src/test/java/customarraylist/TddCustomArrayListImplementationTest.java
@@ -1,10 +1,11 @@
 package customarraylist;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static java.util.stream.IntStream.range;
+import static org.apache.commons.lang3.RandomStringUtils.random;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TddCustomArrayListImplementationTest {
@@ -24,8 +25,81 @@ class TddCustomArrayListImplementationTest {
     @Test
     void listWithOneElementHasSizeOne(){
         List<String> list =new TddArrayList<>();
-        list.add(RandomStringUtils.random(10));
+        list.add(random(10));
         assertThat(list).hasSize(1);
     }
+
+    @Test
+    void canGetElementFromList(){
+        List<Integer> list =new TddArrayList<>();
+        list.add(6);
+        list.add(7);
+
+        assertThat(list.get(1)).isEqualTo(7);
+    }
+
+    @Test
+    void canAddElementAtBeginningOfList(){
+        List<Integer> list =new TddArrayList<>();
+        list.add(6);
+        list.add(0,7);
+
+        assertThat(list).containsExactly(7,6);
+    }
+
+    @Test
+    void canRemoveElementsFromList(){
+        List<String> list = new TddArrayList<>();
+
+        String element = random(10);
+        list.add(element);
+        list.add(random(10));
+
+        list.remove(1);
+        assertThat(list).containsExactly(element);
+
+    }
+
+    @Test
+    void canRemoveElementsFromBeginningOfList(){
+        List<String> list = new TddArrayList<>();
+
+        String element = random(10);
+
+        list.add(random(10));
+        list.add(element);
+
+
+        list.remove(0);
+        assertThat(list).containsExactly(element);
+    }
+
+    @Test
+    void removeReturnTheRemovedElement() {
+        List<String> list = new TddArrayList<>();
+
+        String element = random(10);
+
+        list.add(random(10));
+        list.add(element);
+
+        assertThat(list.remove(1)).isSameAs(element);
+
+    }
+
+    @Test
+    void canRemoveFirstElementInArrayListFullCapacity() {
+        TddArrayList<String> list = new TddArrayList<>();
+
+        range(0, list.currentCapacity()).forEach(__ -> list.add(random(10)));
+
+        assertThat(list).hasSize(list.currentCapacity());
+
+        list.remove(0);
+
+        assertThat(list).hasSize(list.currentCapacity()-1);
+
+    }
+
 
 }
